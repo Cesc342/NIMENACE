f6cfb0667b644f1cb7806388c4aa4e4631f106b0
Cesc342 <francescferrer1@gmail.com>
2025-10-08 10:49:30 +0200

    2

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 const EXPRESS = require("express");
 const app = EXPRESS();
 const path = require("path")
-const server = require("http").createServer(app);
-const io = require("socket.io")(server);
 
-io.on("connection", () => {
-    console.log("HEY");
+
+app.get("/db",(req, res) => {
+    
 })
 
 app.use( EXPRESS.static( path.join(__dirname + "/frontend") ) );
 
-app.listen(80);
+app.listen(80, "localhost", () => {
+    console.log("CONNECTAT");
+});
