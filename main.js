f2d2a7537c54ae5f1d05792907810d2dabe83074
Cesc342 <francescferrer1@gmail.com>
2025-10-08 12:59:02 +0200

    Base Dades Comensat

--- main.js
+++ main.js
@@ -1,10 +1,47 @@
 const EXPRESS = require("express");
-const app = EXPRESS();
-const path = require("path")
+const SOCKETIO = require("socket.io");
+const HTTP = require("http");
 
+const app = EXPRESS();
+const server = HTTP.createServer(app);
+const io = SOCKETIO(server);
+
+const path = require("path")
+const fs = require("fs")
+
+///////////////////////////////////// BASE DADES ////////////////////////////////
+
+class BaseDades{
+    constructor(nom) {
+        this.nom = nom;
+        let json_str = "";
+        json_str = fs.readFileSync(__dirname + `/${nom}.json`).toString();
+
+        this.json = JSON.parse(json_str);
+
+        console.table(this.json);
+    }
+
+    load(){
+        json_str = fs.readFileSync(__dirname + `/${nom}.json`).toString();
+        this.json = JSON.parse(json_str);
+        console.log(`Base de dades ${this.nom} llegit`);
+    }
+
+    save() {
+        json_str = JSON.stringify(this.json);
+        fs.writeFileSync(__dirname + `/${nom}.json`, json_str);
+        console.log(`Base de dades ${this.nom} guardat`);
+    }
+}
+
+let bd = new BaseDades("db");
+
+
+///////////////////////////////////// SERVIDOR //////////////////////////////////
 
 app.get("/db",(req, res) => {
-    
+    res.json(baseDades);
 })
 
 app.use( EXPRESS.static( path.join(__dirname + "/frontend") ) );
@@ -12,3 +49,4 @@ app.use( EXPRESS.static( path.join(__dirname + "/frontend") ) );
 app.listen(80, "localhost", () => {
     console.log("CONNECTAT");
 });
+
