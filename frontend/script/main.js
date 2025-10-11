210188e5d80da0a7f32f095374eb06b15ff388a8
Cesc342 <francescferrer1@gmail.com>
2025-10-11 21:15:59 +0200

    Formateig maquina per la base de dades

--- frontend/script/main.js
+++ frontend/script/main.js
@@ -2,7 +2,7 @@ function load(){
     let columnes = document.getElementsByClassName("columna");
 
     for(let i = 0; i < columnes.length; i++){
-        columnes[i].style.left = `${i*20}%`;
+        columnes[i].style.left = `${i*25}%`;
     }
 }
 
--- main.js
+++ main.js
@@ -20,7 +20,9 @@ class BaseDades{
 
         this.json = JSON.parse(json_str);
 
-        console.table(this.json);
+        for(let x in this.json){
+            console.table(this.json[x]);
+        }
     }
 
     load(){
@@ -43,6 +45,54 @@ bd.json.a = Math.round( Math.random() * 20);
 
 bd.save();
 
+///////////////////////////////////// PROCESSAMENT JOC //////////////////////////
+
+/*
+    ·   a --> 1: 0bx
+    ··  b --> 2: 0bxx               Byte Estat: abbccddd
+    ··· c --> 3: 0bxx               (0 - 255)
+    ····d --> 4: 0bxxx
+
+    ·   1 --> 1,2,3,4 : 0bxx : n(numero peces tretes)
+    ··  2 --> 1,2,3,4 : =           Byte Decisio (seguent estat): ccnn
+    ··· 3 --> 1,2,3,4 : =           (0 - 15)
+    ····4 --> 1,2,3,4 : =
+        0bxx : c(olumna)
+*/
+
+const NUM_FITXES = 10;
+
+
+function reiniciarMaquina(baseDades){
+    let dades = baseDades.json;
+    let estatInternReiniciat = [];
+
+    for(let i = 0; i < 16; i++) estatInternReiniciat.push(NUM_FITXES);
+
+    dades.maquina = [];
+    console.log("=================")
+    for(let a = 0; a < 2; a++){
+        for(let b = 0; b < 3; b++){
+            for(let c = 0; c < 4; c++){
+                for(let d = 0; d < 5; d++){
+                    
+                    let i = (a) | (b << 1) | (c << 3) | (d<<5);
+                    dades.maquina[i] = estatInternReiniciat;
+                    console.log(`${a},${b},${c},${d} :: ${i}`);
+                }
+            }
+        }
+    }
+    console.log("=================")
+
+    baseDades.json = dades;
+    baseDades.json["Partides Guanyades"] = 0;
+    console.log(estatInternReiniciat);
+}
+
+reiniciarMaquina(bd);
+console.log(bd.json.maquina);
+bd.save();
 
 ///////////////////////////////////// SERVIDOR //////////////////////////////////
 
@@ -59,5 +109,10 @@ app.listen(PORT, HOST, () => {
     console.log(`Servidor connectat a http://${HOST}:${PORT}`);
 });
 
+app.post("/reiniciar", (req,res) => {
+    if(req.body.contra == "contrasenya"){
+        
+    }else{
 
-
+    }
+})
